7c18033e9d4c41d16cb304670010f64b0b0cca50
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-01-07 14:00:06 +0100

    handle ctrl C need to fix ctrl D

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include "shell.h"
+#include <signal.h>
+
+void handle_sigint(int sig);
 
 /**
  * main - Entry point of the shell program
@@ -12,10 +15,23 @@ int main(int argc, char **argv, char **env)
 {
 	(void)argc;
 
+	/* Set up signal handler for SIGINT (Ctrl+C) */
+	signal(SIGINT, handle_sigint);
+
 	if (isatty(STDIN_FILENO))
 		interactive_shell(argv, env);
 	else
 		non_interactive_shell(argv, env);
 
 	return (0);
+}
+
+/**
+ * handle_sigint - Signal handler for SIGINT (Ctrl+C)
+ * @sig: Signal number (unused)
+ */
+void handle_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n($) ", 5);
 }
